086f117c254f2c31663847626c7343e13e4c4eae
Vassily Ivanov <vivanov@aeb-it.ru>
2018-05-16 16:46:34 +0900

    Added IDbContextModule

---
+++ Cynosura.EF/IDbContextModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cynosura.EF
+{
+    public interface IDbContextModule
+    {
+        void CreateModel(ModelBuilder builder);
+    }
+}
