c11d4e9368c49ba2d60372df9358073377f0f0b1
Vassily Ivanov <vivanov@aeb-it.ru>
2018-05-17 16:12:33 +0900

    Added Autofac to Web

---
+++ Cynosura.Web/Autofac/WebModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Cynosura.Web.Infrastructure;
+
+namespace Cynosura.Web.Autofac
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApiExceptionFilterAttribute>();
+        }
+    }
+}
