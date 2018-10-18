15ba416191454b41c4519669c3621722dabf17f8
Anatoliy Mikhailov <yamaoto@mail.ru>
2018-10-18 21:26:43 +0900

    CYN-14 Add buildReleaseNuGetPackages for non-Windows platforms

---
+++ buildReleaseNuGetPackages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dotnet restore
+
+dotnet pack Cynosura.Core --configuration Release --force --output ../artifacts
+
+dotnet pack Cynosura.EF --configuration Release --force --output ../artifacts
+
+dotnet pack Cynosura.Web --configuration Release --force --output ../artifacts
